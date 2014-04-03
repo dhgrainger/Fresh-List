6530aebfaf15d9a1ad3217f78e41199029d20eb7
Doug Grainger <dhgrainger@gmail.com>
2014-04-03 14:35:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreshList::Application.config.secret_key_base = 'a9e533912a4870a4a6dbe6d76037387c7b0eef36bc6cb3f581086145e7e3e4c6956dfbf40dd297f03968abb43fae1ebf98a5b433edf1231be5b2dda35440d938'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FreshList::Application.config.session_store :cookie_store, key: '_fresh_list_session'
