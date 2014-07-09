fbc818a75982013b0d0abe9530744a243037e6d9
Doug Grainger <dhgrainger@gmail.com>
2014-07-09 13:37:54 -0400

    making new branch

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,59 +13,75 @@ class Recipe < ActiveRecord::Base
 
   class << self
     def search(search)
-
       results = all.where('to_tsvector(name) @@ plainto_tsquery(?)', search)
-
       # if the database doesnt include the search term we are going to query the yummly api
       if results.map {|x| x[:name].downcase.include? search}.include? true
         return results
       else
-
-      # we need to make sure the data is in the right format for the search
-        if search.include? ' '
-          params = search.split(' ').join('+')
-        else
-          params = search
-        end
-      # this next block is going to search recipes and return 10 recipes that match our search
-
-        source = 'http://api.yummly.com/v1/api/recipes?_app_id=76673592&_app_key=17ee3cd3288f06af85bc442278910238&q=' + params + '&requirePictures=true'
-        resp = Net::HTTP.get_response(URI.parse(source))
-        data = resp.body
-        yummly = JSON.parse(data)
-
-      # this next block then queries the yummly api again to get more data including nutritional info from the api for each recipe
-        yummly["matches"].each do |yum|
-
-          yumId = yum["id"]
-          source = 'http://api.yummly.com/v1/api/recipe/' + yumId + '?_app_id=76673592&_app_key=17ee3cd3288f06af85bc442278910238&'
-          resp = Net::HTTP.get_response(URI.parse(source))
-          data = resp.body
-          info = JSON.parse(data)
-
-          recipe = Recipe.new
-
-          recipe.name = yum["recipeName"]
-          recipe.image = yum["smallImageUrls"][0]
-          recipe.url = 'http://www.yummly.com/recipe/' + yum["id"]
-
-      # this next block returns carbs protein and fats in grams
-          info["nutritionEstimates"].each do |attribute|
-            if attribute["description"] == 'Carbohydrate, by difference'
-              recipe.carbs = attribute["value"].round(0)
-            end
-            if attribute["description"] == 'Protein'
-              recipe.protein = attribute["value"].round(0)
-            end
-            if attribute["description"] == 'Total lipid (fat)'
-              recipe.fats = attribute["value"].round(0)
-            end
-          end
-          recipe.save
-        end
-         results = all.where('to_tsvector(name) @@ plainto_tsquery(?)', search)
-        results
+        clean(search)
       end
     end
+
+    # we need to make sure the data is in the right format for the search
+    def clean(search)
+      if search.include? ' '
+        params = search.split(' ').join('+')
+      else
+        params = search
+      end
+      yummly_recipes(params)
+    end
+
+    # this next block is going to search recipes and return 10 recipes that match our search
+    def yummly_recipes(params)
+      source = 'http://api.yummly.com/v1/api/recipes?_app_id=76673592&_app_key=17ee3cd3288f06af85bc442278910238&q=' + params + '&requirePictures=true'
+      resp = Net::HTTP.get_response(URI.parse(source))
+      data = resp.body
+      recipes = JSON.parse(data)
+      yummly_ingredients(recipes)
+    end
+
+    # this next block then queries the yummly api again to get more data including nutritional info from the api for each recipe
+    def yummly_ingredients(recipes)
+      recipes["matches"].each do |plate|
+        binding.pry
+        recipe_id = plate["id"]
+        source = 'http://api.yummly.com/v1/api/recipe/' + recipe_id + '?_app_id=76673592&_app_key=17ee3cd3288f06af85bc442278910238&'
+        resp = Net::HTTP.get_response(URI.parse(source))
+        data = resp.body
+        info = JSON.parse(data)
+        new_recipe(info, plate)
+      end
+    end
+
+    #i need to look up a more graceful way of creating new objects
+    def new_recipe(info, plate)
+      binding.pry
+      recipe = Recipe.new
+      recipe.name = plate["recipeName"]
+      binding.pry
+      recipe.image = plate["smallImageUrls"][0]
+      recipe.url = 'http://www.yummly.com/recipe/' + plate["id"]
+      nutrition_estimates(info, recipe)
+    end
+
+    # this next block returns carbs protein and fats in grams
+    def nutrition_estimates(info, recipe)
+      binding.pry
+      info["nutritionEstimates"].each do |attribute|
+        if attribute["description"] == 'Carbohydrate, by difference'
+          recipe.carbs = attribute["value"].round(0)
+        end
+        if attribute["description"] == 'Protein'
+          recipe.protein = attribute["value"].round(0)
+        end
+        if attribute["description"] == 'Total lipid (fat)'
+          recipe.fats = attribute["value"].round(0)
+        end
+      end
+      recipe.save
+      search(recipe)
+    end
+
   end
 end
